93c940df8ce1e739b01c8ec36592aba782c74053
Letha <lev1er@protonmail.com>
2019-11-13 09:10:25 +0000

    Update routes.

--- app/src/config/routes.php
+++ app/src/config/routes.php
@@ -3,11 +3,18 @@
      * routes to controllers with their actions
      */
     return array(
-        // First key means regular expression for client's query string.
-        // First value:
-        // - first part means method actionIndex
-        // - second part means class IndexController of that method.
-        // And so on for other elements.
-        // Parts of value string after second slash means query parameters.
-        '^$' => 'index/index'
+        /**
+        * First key means regular expression for client's query string.
+        * First value:
+        * - first part means class IndexController;
+        * - second part means method actionIndex of that class.
+        * And so on for other elements.
+        * Parts of value string after second slash means query parameters.
+        */
+        '^$' => 'index/index',
+        '^ajax/login$' => 'enter/login',
+        '^ajax/register$' => 'enter/register',
+        '^ajax/exit$' => 'enter/exit',
+        '^ajax/language$' => 'configuration/language',
+        '^ajax/image/personal-photo$' => 'loadFiles/uploadPersonalPhoto'
     );
