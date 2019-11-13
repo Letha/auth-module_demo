7da2fed67dce4951fb218802be4ec9cf3582d1a9
Letha <lev1er@protonmail.com>
2019-11-13 07:43:01 +0000

    Delete extra files.

--- app/src/Components/Exceptions/DictionaryException.php
+++
@@ -1,6 +0,0 @@
-<?php
-    namespace App\Components\Exceptions;
-    use App\Components\AppException;
-    class DictionaryException extends AppException
-    {
-    }
--- app/src/Components/Exceptions/RouterException.php
+++
@@ -1,6 +0,0 @@
-<?php
-    namespace App\Components\Exceptions;
-    use App\Components\AppException;
-    class RouterException extends AppException
-    {
-    }
